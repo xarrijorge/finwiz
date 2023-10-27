dfb4154a98668116aa4cd262a7799a71ff900a92
Xarri Jorge <kodinglife@gmail.com>
2023-10-27 09:40:14 +0000

    initial workflow completed

--- src/components/CreditScore.jsx
+++ src/components/CreditScore.jsx
@@ -1,9 +1,80 @@
-import React from 'react'
+import React, { useState } from 'react';
+import Avatar from '@mui/material/Avatar';
+import Button from '@mui/material/Button';
+import CssBaseline from '@mui/material/CssBaseline';
+import TextField from '@mui/material/TextField';
+import Box from '@mui/material/Box';
+import GradingIcon from '@mui/icons-material/Grading';
+import Typography from '@mui/material/Typography';
+import Container from '@mui/material/Container';
+import { Link as NavLink } from 'react-router-dom';
 
-const CreditScore = () => {
+export default function CreateScore({createData}) {
+  const [record, setRecord] = useState({});
+ 
   return (
-    <h1>CreditScore</h1>
-  )
-}
+    <Container component='main' maxWidth='xs'>
+      <CssBaseline />
+      <Box
+        sx={{
+          marginTop: 8,
+          display: 'flex',
+          flexDirection: 'column',
+          alignItems: 'center',
+        }}>
+        <Avatar sx={{ m: 1, bgcolor: 'royalblue' }}>
+          <GradingIcon />
+        </Avatar>
+        <Typography component='h1' variant='h5'>
+          Please Enter Your Information
+        </Typography>
+        <Typography component='p'>
+          Please input all data in numeric format
+        </Typography>
+        <Box component='form' noValidate sx={{ mt: 1 }}>
+          <TextField
+            margin='normal'
+            required
+            fullWidth
+            id='email'
+            label='Sales Data'
+            name='sales'
+            autoComplete='sales'
+            autoFocus
+          />
+          <TextField
+            margin='normal'
+            required
+            fullWidth
+            name='expense'
+            label='Expense Data'
+            type='text'
+            id='expense'
+            autoComplete='expense'
+          />
+          <TextField
+            margin='normal'
+            required
+            fullWidth
+            name='expense'
+            label='Location'
+            type='text'
+            id='expense'
+            autoComplete='expense'
+          />
 
-export default CreditScore
+         <span>
+            <Button
+              onClick={createData}
+              fullWidth
+              variant='contained'
+              sx={{ mt: 3, mb: 2 }}>
+              Record
+            </Button>
+            <a href='/generate'>Go back to Home</a>
+            </span>
+        </Box>
+      </Box>
+    </Container>
+  );
+}
--- src/components/Generate.jsx
+++ src/components/Generate.jsx
@@ -1,7 +1,24 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
+import Button from '@mui/material/Button';
 
-export const Generate = () => {
-  return <div>{/* <Link to='/creditscore'>Generate</Link> */}</div>;
+export const Generate = ({ user = 'Broomstick', score = +10 }) => {
+  return (
+    <div className='generate'>
+      <h1>Hello {user}</h1>
+      <h3>Your current credit score is {score}</h3>
+      <Link to='/creditscore'>
+        <p>
+          <Button variant='contained'>Add Transaction</Button>
+        </p>
+        <p>
+          <Button variant='contained' color='red'>
+            Link an ABSA Account
+          </Button>
+        </p>
+      </Link>
+    </div>
+  );
 };
 
 export default Generate;
