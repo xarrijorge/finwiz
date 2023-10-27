ed86ee3482400c7cee1c0ff39a2ab713c7aea9c4
Xarri Jorge <kodinglife@gmail.com>
2023-10-27 10:24:07 +0000

    Moved nav to App. Added netlify.toml

--- src/App.js
+++ src/App.js
@@ -1,10 +1,23 @@
+import React, { useState } from 'react';
 import './App.css';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 import SignIn from './components/Login';
+import Generate from './components/Generate';
+import CreditScore from './components/CreditScore';
 
 
 function App() {
   return (
-    <SignIn />
+    <>
+      <Router>
+        <Routes>
+          <Route path="/" element={<SignIn />} />
+          <Route path="/generate" element={<Generate />} />
+          <Route path="/creditscore" element={<CreditScore />} />
+        </Routes>
+      </Router>
+
+    </>
   );
 }
 
