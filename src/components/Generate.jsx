6da2cb11a7ff4a5688a95a43315246fa73964949
Xarri Jorge <kodinglife@gmail.com>
2023-10-27 10:06:30 +0000

    Change color of link button

--- src/components/Generate.jsx
+++ src/components/Generate.jsx
@@ -2,21 +2,23 @@ import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import Button from '@mui/material/Button';
 
-export const Generate = ({ user = 'Broomstick', score = +10 }) => {
+export const Generate = ({ user = 'Alex', score = +10 }) => {
   return (
     <div className='generate'>
-      <h1>Hello {user}</h1>
+      <h1>Welcome, {user}</h1>
       <h3>Your current credit score is {score}</h3>
       <Link to='/creditscore'>
         <p>
           <Button variant='contained'>Add Transaction</Button>
         </p>
-        <p>
-          <Button variant='contained' color='red'>
+      </Link>
+      <p>
+        <Link to='https://www.absa.com.gh/personal/'>
+          <Button variant='contained' color='error'>
             Link an ABSA Account
           </Button>
-        </p>
-      </Link>
+        </Link>
+      </p>
     </div>
   );
 };
